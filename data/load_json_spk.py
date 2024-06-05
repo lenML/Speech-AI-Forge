6c4aa29f147942d1c2f3c095b4d832409e53e5cb
zhzluke96 <zhzluke96@outlook.com>
2024-06-06 00:58:10 +0800

    :sparkles: add speakers

---
+++ data/load_json_spk.py
@@ -0,0 +1,20 @@
+import json
+from modules.speaker import speaker_mgr
+
+
+data = json.load(open("./data/slct_voice_240605.json", "r"))
+
+print("load speakers: ", len(data))
+
+for id, spk in data.items():
+    print(id, spk["describe"])
+    name = f"spk_{id}"
+    describe = spk["describe"]
+    gender = spk["gender"]
+    speaker_mgr.create_speaker_from_tensor(
+        filename=name,
+        name=describe,
+        describe=describe,
+        gender=gender,
+        tensor=spk["tensor"],
+    )
