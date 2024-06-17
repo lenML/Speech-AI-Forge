206fabc76f1dbad261c857cb02f8c99c21e99eef
zhzluke96 <zhzluke96@outlook.com>
2024-06-17 16:24:35 +0800

    :zap: improve xtts_v2 api

--- modules/api/impl/xtts_v2_api.py
+++ modules/api/impl/xtts_v2_api.py
@@ -30,8 +30,19 @@ class XTTS_V2_Settings:
         self.top_k = 20
         self.enable_text_splitting = True
 
+        # 下面是额外配置 xtts_v2 中不包含的，但是本系统需要的
+        self.batch_size = 4
+        self.eos = "[uv_break]"
+        self.infer_seed = 42
+        self.use_decoder = True
+        self.prompt1 = ""
+        self.prompt2 = ""
+        self.prefix = ""
+        self.spliter_threshold = 100
+
 
 class TTSSettingsRequest(BaseModel):
+    # 这个 stream_chunk 现在当作 spliter_threshold 用
     stream_chunk_size: int
     temperature: float
     speed: float
@@ -40,6 +51,15 @@ class TTSSettingsRequest(BaseModel):
     top_p: float
     top_k: int
     enable_text_splitting: bool
+
+    batch_size: int = None
+    eos: str = None
+    infer_seed: int = None
+    use_decoder: bool = None
+    prompt1: str = None
+    prompt2: str = None
+    prefix: str = None
+    spliter_threshold: int = None
 
 
 class SynthesisRequest(BaseModel):
@@ -79,17 +99,22 @@ def setup(app: APIManager):
 
         text = text_normalize(text, is_end=True)
         sample_rate, audio_data = synthesize_audio(
-            text=text,
-            temperature=XTTSV2.temperature,
+            # TODO: 这两个参数现在用不着...但是其实gpt是可以用的
             # length_penalty=XTTSV2.length_penalty,
             # repetition_penalty=XTTSV2.repetition_penalty,
+            text=text,
+            temperature=XTTSV2.temperature,
             top_P=XTTSV2.top_p,
             top_K=XTTSV2.top_k,
             spk=spk,
-            spliter_threshold=XTTSV2.stream_chunk_size,
-            # TODO 支持设置 batch_size
-            batch_size=4,
-            end_of_sentence="[uv_break]",
+            spliter_threshold=XTTSV2.spliter_threshold,
+            batch_size=XTTSV2.batch_size,
+            end_of_sentence=XTTSV2.eos,
+            infer_seed=XTTSV2.infer_seed,
+            use_decoder=XTTSV2.use_decoder,
+            prompt1=XTTSV2.prompt1,
+            prompt2=XTTSV2.prompt2,
+            prefix=XTTSV2.prefix,
         )
 
         if XTTSV2.speed:
@@ -145,6 +170,8 @@ def setup(app: APIManager):
                 )
 
             XTTSV2.stream_chunk_size = request.stream_chunk_size
+            XTTSV2.spliter_threshold = request.stream_chunk_size
+
             XTTSV2.temperature = request.temperature
             XTTSV2.speed = request.speed
             XTTSV2.length_penalty = request.length_penalty
@@ -152,6 +179,25 @@ def setup(app: APIManager):
             XTTSV2.top_p = request.top_p
             XTTSV2.top_k = request.top_k
             XTTSV2.enable_text_splitting = request.enable_text_splitting
+
+            # TODO: checker
+            if request.batch_size:
+                XTTSV2.batch_size = request.batch_size
+            if request.eos:
+                XTTSV2.eos = request.eos
+            if request.infer_seed:
+                XTTSV2.infer_seed = request.infer_seed
+            if request.use_decoder:
+                XTTSV2.use_decoder = request.use_decoder
+            if request.prompt1:
+                XTTSV2.prompt1 = request.prompt1
+            if request.prompt2:
+                XTTSV2.prompt2 = request.prompt2
+            if request.prefix:
+                XTTSV2.prefix = request.prefix
+            if request.spliter_threshold:
+                XTTSV2.spliter_threshold = request.spliter_threshold
+
             return {"message": "Settings successfully applied"}
         except Exception as e:
             if isinstance(e, HTTPException):
