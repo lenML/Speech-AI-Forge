85c98f69c149863fb7d6bb962110cdd034830885
zhzluke96 <zhzluke96@outlook.com>
2024-06-27 16:02:06 +0800

    :bug: fix Including `&` escape char causes normalization errors #77
    - 改用 正则 而不是 HTMLParser

--- modules/utils/html.py
+++ modules/utils/html.py
@@ -1,6 +1,10 @@
 from html.parser import HTMLParser
+import html
+import re
 
 
+# NOTE: 现在没用这个，因为不好解决转义字符的问题
+#       除非分段处理，但是太麻烦了...
 class HTMLTagRemover(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -20,7 +24,19 @@ def remove_html_tags(text):
     return parser.get_data()
 
 
+def remove_html_tags_re(text):
+    text = html.unescape(text)
+    html_tags_pattern = re.compile(r"</?([a-zA-Z1-9]+)[^>]*>")
+    return re.sub(html_tags_pattern, " ", text)
+
+
 if __name__ == "__main__":
-    input_text = "<h1>一个标题</h1> 这是一段包含<code>标签</code>的文本。"
-    output_text = remove_html_tags(input_text)
+    input_text = """
+<h1>一个标题</h1> 这是一段包含<code>标签</code>的文本。 <code>&amp;</code>
+<设定>
+一些文本
+</设定>
+"""
+    # input_text = "我&你"
+    output_text = remove_html_tags_re(input_text)
     print(output_text)  # 输出： 一个标题 这是一段包含标签的文本。
