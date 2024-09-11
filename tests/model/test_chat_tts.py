4d9d671e08958d433bde408be9304c82d4d51032
zhzluke96 <zhzluke96@outlook.com>
2024-09-11 15:21:28 +0800

    :white_check_mark: add model tests
    - 增加 model 相关代码测试

---
+++ tests/model/test_chat_tts.py
@@ -0,0 +1,12 @@
+
+import pytest
+
+from modules.core.models.tts.ChatTtsModel import ChatTTSModel
+from modules.core.spk.TTSSpeaker import TTSSpeaker
+
+@pytest.mark.model_chat_tts
+def test_create_speaker_from_seed():
+    # NOTE: 主要是测试 chat tts 官方库有没有修改，可能导致这个函数错误 
+    spk = ChatTTSModel.create_speaker_from_seed(42)
+    assert isinstance(spk, TTSSpeaker)
+    
