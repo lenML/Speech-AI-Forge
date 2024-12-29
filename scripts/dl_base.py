07d0a2394f78a3a34e067d83763c4614ed218bae
zhzluke96 <zhzluke96@outlook.com>
2024-12-30 00:12:30 +0800

    feat: add cosyvoice2 downloader

--- scripts/dl_base.py
+++ scripts/dl_base.py
@@ -7,16 +7,27 @@ from scripts.ModelDownloader import ModelDownloader
 
 
 class BaseModelDownloader(ModelDownloader):
+
     def __init__(
-        self, model_name, modelscope_repo=None, huggingface_repo=None, required_files=[]
+        self,
+        model_name,
+        modelscope_repo=None,
+        huggingface_repo=None,
+        required_files=[],
+        modelscope_revision="master",
+        huggingface_revision="main",
+        just_download_required_files=False,
     ):
         super().__init__()
         self.model_name = model_name
         self.modelscope_repo: Optional[str] = modelscope_repo
         self.huggingface_repo: Optional[str] = huggingface_repo
+        self.modelscope_revision = modelscope_revision
+        self.huggingface_revision = huggingface_revision
         self.required_files = required_files
         self.model_dir = self.dir_path / model_name
         self.cache_dir = self.dir_path / "cache"
+        self.just_download_required_files = just_download_required_files
 
         if not self.dir_path.exists():
             self.dir_path.mkdir(parents=True)
@@ -25,16 +36,26 @@ class BaseModelDownloader(ModelDownloader):
         if not self.cache_dir.exists():
             self.cache_dir.mkdir(parents=True)
 
-    def from_modelscope(self):
-        if self.modelscope_repo is None:
-            raise Exception(
-                "This downloader does not support downloading from ModelScope."
-            )
+    def from_modelscope_just_requires(self):
+        from modelscope import model_file_download
 
+        for file in self.required_files:
+            try:
+                downloaded_file = model_file_download(
+                    model_id=self.modelscope_repo,
+                    file_path=file,
+                    revision=self.modelscope_revision,
+                    cache_dir=str(self.cache_dir),
+                )
+                shutil.move(downloaded_file, self.model_dir / file)
+                self.logger.info(f"Downloaded {file} from ModelScope.")
+            except Exception as e:
+                self.logger.error(f"Failed to download {file} from ModelScope: {e}")
+
+    def from_modelscope_repo(self):
         from modelscope import snapshot_download
 
         snapshot_download(self.modelscope_repo, cache_dir=self.cache_dir)
-
         # Move files to the model directory
         from_path = self.cache_dir / self.modelscope_repo.replace(".", "___")
         to_path = self.model_dir
@@ -46,16 +67,23 @@ class BaseModelDownloader(ModelDownloader):
                     print(f"Failed to move {item}: {e}")
         else:
             os.system(f"mv {from_path}/* {to_path}/")
+
+    def from_modelscope(self):
+        if self.modelscope_repo is None:
+            raise Exception(
+                "This downloader does not support downloading from ModelScope."
+            )
+
+        if self.just_download_required_files:
+            self.from_modelscope_just_requires()
+        else:
+            self.from_modelscope_repo()
+
         self.logger.info(
             f"Model downloaded from ModelScope successfully, saved at: {self.model_dir}"
         )
 
-    def from_huggingface(self):
-        if self.huggingface_repo is None:
-            raise Exception(
-                "This downloader does not support downloading from HuggingFace."
-            )
-
+    def from_huggingface_repo(self):
         from huggingface_hub import snapshot_download
 
         snapshot_download(
@@ -64,6 +92,33 @@ class BaseModelDownloader(ModelDownloader):
             local_dir=self.model_dir,
             local_dir_use_symlinks=False,
         )
+
+    def from_huggingface_just_requires(self):
+        from huggingface_hub import hf_hub_download
+
+        for file in self.required_files:
+            try:
+                downloaded_file = hf_hub_download(
+                    repo_id=self.huggingface_repo,
+                    filename=file,
+                    revision=self.huggingface_revision,
+                    cache_dir=str(self.cache_dir),
+                )
+                shutil.move(downloaded_file, self.model_dir / file)
+                self.logger.info(f"Downloaded {file} from HuggingFace.")
+            except Exception as e:
+                self.logger.error(f"Failed to download {file} from HuggingFace: {e}")
+
+    def from_huggingface(self):
+        if self.huggingface_repo is None:
+            raise Exception(
+                "This downloader does not support downloading from HuggingFace."
+            )
+        if self.just_download_required_files:
+            self.from_huggingface_just_requires()
+        else:
+            self.from_huggingface_repo()
+
         self.logger.info(
             f"Model downloaded from HuggingFace successfully, saved at: {self.model_dir}"
         )
---
+++ scripts/downloader/cosyvoice2.py
@@ -0,0 +1,42 @@
+import logging
+
+from scripts.dl_base import BaseModelDownloader
+
+logger = logging.getLogger(__name__)
+
+
+class CosyVoice2Downloader(BaseModelDownloader):
+    def __init__(self):
+        required_files = [
+            "campplus.onnx",
+            "configuration.json",
+            "cosyvoice.yaml",
+            "flow.pt",
+            "hift.pt",
+            "llm.pt",
+            "speech_tokenizer_v2.onnx",
+            "CosyVoice-BlankEN/model.safetensors",
+            "CosyVoice-BlankEN/config.json",
+            "CosyVoice-BlankEN/generation_config.json",
+            "CosyVoice-BlankEN/merges.txt",
+            "CosyVoice-BlankEN/tokenizer_config.json",
+            "CosyVoice-BlankEN/vocab.json",
+        ]
+        super().__init__(
+            model_name="CosyVoice2-0.5B",
+            modelscope_repo="iic/CosyVoice2-0.5B",
+            huggingface_repo="FunAudioLLM/CosyVoice2-0.5B",
+            required_files=required_files,
+            just_download_required_files=True,
+        )
+        self.logger = logger
+
+        self.blank_dir = self.model_dir / "CosyVoice-BlankEN"
+        self.blank_dir.mkdir(parents=True, exist_ok=True)
+
+
+if __name__ == "__main__":
+    from scripts.dl_args import parser_args
+
+    args = parser_args()
+    CosyVoice2Downloader()(source=args.source)
