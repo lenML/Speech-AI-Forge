40da1ea55d57f6c85815dcbf6212c5869ba3cae2
nickchen110 <jerrychen110@hotmail.com>
2025-05-08 10:06:09 +0800

    add index tts module

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -8,6 +8,7 @@ except:
 from scripts.dl_args import parser_args
 from scripts.dl_chattts import ChatTTSDownloader
 from scripts.dl_enhance import ResembleEnhanceDownloader
+from scripts.dl_cosyvoice import CosyVoiceDownloader
 from scripts.dl_cosyvoice_base import CosyVoiceBaseDownloader
 from scripts.dl_cosyvoice_instruct import CosyVoiceInstructDownloader
 from scripts.dl_f5_tts import F5TTSDownloader
@@ -17,6 +18,7 @@ from scripts.dl_fish_speech_1_2sft import FishSpeechDownloader
 from scripts.dl_fish_speech_1_4 import FishSpeech14Downloader
 from scripts.dl_open_voice import OpenVoiceDownloader
 from scripts.dl_vocos_mel_24khz import VocosMel24khzDownloader
+from scripts.dl_index_tts import IndexTTSDownloader
 
 from scripts.ModelDownloader import ModelDownloader
 
@@ -36,6 +38,7 @@ def main():
     downloaders.append(FishSpeech14Downloader())
     downloaders.append(OpenVoiceDownloader())
     downloaders.append(VocosMel24khzDownloader())
+    downloaders.append(IndexTTSDownloader())
 
     for downloader in downloaders:
         downloader(source=args.source)
