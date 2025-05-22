fd04a48ba99a0cdfc92067f0ae3222b3f3fca43c
zhzluke96 <zhzluke96@outlook.com>
2025-05-22 21:58:42 +0800

    feat: add GptSoVitsV4Downloader #198

---
+++ scripts/downloader/gpt_sovits_v4.py
@@ -0,0 +1,36 @@
+import logging
+
+from scripts.dl_base import BaseModelDownloader
+
+logger = logging.getLogger(__name__)
+
+
+class GptSoVitsV4Downloader(BaseModelDownloader):
+    def __init__(self):
+        required_files = [
+            "chinese-hubert-base/config.json",
+            "chinese-hubert-base/preprocessor_config.json",
+            "chinese-hubert-base/pytorch_model.bin",
+            "chinese-roberta-wwm-ext-large/config.json",
+            "chinese-roberta-wwm-ext-large/tokenizer.json",
+            "chinese-roberta-wwm-ext-large/pytorch_model.bin",
+            "gsv-v4-pretrained/s2Gv4.pth",
+            "gsv-v4-pretrained/vocoder.pth",
+            "s1v3.ckpt",
+        ]
+        super().__init__(
+            model_name="gpt_sovits_v4",
+            modelscope_repo="AI-ModelScope/GPT-SoVITS",
+            huggingface_repo="lj1995/GPT-SoVITS",
+            required_files=required_files,
+            just_download_required_files=True,
+        )
+
+        self.logger = logger
+
+
+if __name__ == "__main__":
+    from scripts.dl_args import parser_args
+
+    args = parser_args()
+    GptSoVitsV4Downloader()(source=args.source)
