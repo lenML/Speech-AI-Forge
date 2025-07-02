030531d930b27da1ba866e436f12840051dd24ac
zhzluke96 <zhzluke96@outlook.com>
2025-07-03 00:20:48 +0800

    feat: improve STT pipeline

--- modules/core/models/stt/STTChunker.py
+++ modules/core/models/stt/STTChunker.py
@@ -26,6 +26,10 @@ class RefrenceTranscript:
     def __init__(self, content: str):
         self.raw = content
         self.buffer = content
+        self.raw_lines = content.split("\n")
+        self.raw_lines = [
+            line.strip() for line in self.raw_lines if len(line.strip()) > 0
+        ]
 
     def dequeue(self, word: str) -> bool:
         """
@@ -33,6 +37,7 @@ class RefrenceTranscript:
         如果不是则返回 False
         """
         word = word.strip()
+        self.buffer = self.buffer.strip()
         if self.buffer.startswith(word):
             self.buffer = self.buffer[len(word) :].strip()
             return True
@@ -54,7 +59,6 @@ class RefrenceTranscript:
             if not self.dequeue(word):
                 return
 
-
 class STTChunker:
     """
     对于长音频，asr模型一般效果都很差
@@ -65,6 +69,16 @@ class STTChunker:
 
     def __init__(self, model: STTModel):
         self.model = model
+
+    def get_chunk(self, audio: NP_AUDIO, start_s: int, end_s: int):
+        sr, data = audio
+        start = int(start_s * sr)
+        end = int(end_s * sr)
+        return STTChunkData(
+            audio=(sr, data[start:end]),
+            start_s=start_s,
+            end_s=end_s,
+        )
 
     def get_chunks(self, audio: NP_AUDIO):
         """
@@ -98,6 +112,16 @@ class STTChunker:
             speech_duration = get_duration_s(start, end)
 
             if speech_duration >= MAX_DURATION:
+                if buffer_start is not None:
+                    chunks.append(
+                        STTChunkData(
+                            audio=(sr, data[buffer_start:buffer_end]),
+                            start_s=index_to_s(buffer_start),
+                            end_s=index_to_s(buffer_end),
+                        )
+                    )
+                    buffer_start = None
+                    buffer_end = None
                 # 大段，直接作为 chunk
                 chunks.append(
                     STTChunkData(
@@ -138,6 +162,7 @@ class STTChunker:
                 )
             )
 
+        # 根据 start_s 排序
         return chunks
 
     def merge_results(
@@ -170,11 +195,11 @@ class STTChunker:
         return SttResult(
             duration=-1,
             segments=merged_segments,
-            language=results[0].language,
+            language=results[0].language if len(results) != 0 else "zh",
         )
 
     def transcribe_to_result(self, audio: NP_AUDIO, config: STTConfig) -> SttResult:
-        ref_script = RefrenceTranscript(config.refrence_transcript)
+        ref_script = RefrenceTranscript(config.refrence_transcript.strip())
         chunks = self.get_chunks(audio)
         results: list[SttResult] = []
         for chunk in tqdm(
@@ -184,6 +209,37 @@ class STTChunker:
         ):
             config.refrence_transcript = ref_script.buffer
             result = self.model.transcribe_to_result(chunk.audio, config)
+
+            # check last segement if broken
+            # 检查最后一个识别是否是损坏的，如果只识别了一部分，而不是完整的一句，那么我们需要放弃这个识别，并调整下一个 chunk 的数据，包含错误识别的部分
+            while (
+                len(result.segments) != 0
+                and len(ref_script.raw_lines) != 0
+                and result.segments[-1].text not in ref_script.raw_lines
+            ):
+                # TODO: 这个识别好像有点问题，应该关注前一个句子来定位会好点
+                result.segments = result.segments[:-1]
+
+            chunk_idx = chunks.index(chunk)
+            if len(result.segments) == 0:
+                if chunk_idx + 1 < len(chunks):
+                    # 如果这个片段没识别出来或者只能识别一部分，就直接合并到下一个片段
+                    orig_next_chunk = chunks[chunk_idx + 1]
+                    next_chunk = self.get_chunk(
+                        audio=audio, start_s=chunk.start_s, end_s=orig_next_chunk.end_s
+                    )
+                    chunks[chunk_idx + 1] = next_chunk
+                continue
+
+            # 不管有没有 broken 都调整下一个 chunk 的范围
+            next_start_s: int = result.segments[-1].end + chunk.start_s
+            if chunk_idx + 1 < len(chunks):
+                orig_next_chunk = chunks[chunk_idx + 1]
+                next_chunk = self.get_chunk(
+                    audio=audio, start_s=next_start_s, end_s=orig_next_chunk.end_s
+                )
+                chunks[chunk_idx + 1] = next_chunk
+
             results.append(result)
 
             result_content = ""
