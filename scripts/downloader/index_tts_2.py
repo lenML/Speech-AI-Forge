a1e0ce8bab1180730ef448b905b4a65e9d32d353
zhzluke96 <zhzluke96@outlook.com>
2025-09-11 16:50:48 +0800

    feat: indextts-2 downloader #265

---
+++ scripts/downloader/index_tts_2.py
@@ -0,0 +1,30 @@
+import logging
+
+from scripts.dl_base import BaseModelDownloader
+
+logger = logging.getLogger(__name__)
+
+
+class IndexTTS25Downloader(BaseModelDownloader):
+    def __init__(self):
+        # 只对几个大文件进行验证，因为所有文件都需要下载
+        required_files = [
+            "gpt.pth",
+            "s2mel.pth",
+            "qwen0.6bemo4-merge/model.safetensors",
+        ]
+        super().__init__(
+            model_name="Index-TTS-2",
+            modelscope_repo="IndexTeam/IndexTTS-2",
+            huggingface_repo="IndexTeam/IndexTTS-2",
+            required_files=required_files,
+            just_download_required_files=False,
+        )
+        self.logger = logger
+
+
+if __name__ == "__main__":
+    from scripts.dl_args import parser_args
+
+    args = parser_args()
+    IndexTTS25Downloader()(source=args.source)
