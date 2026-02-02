3dadc78dc89bb131e0ca1219d9abc4987c2cf9c1
zhzluke96 <zhzluke96@outlook.com>
2026-02-02 09:58:26 +0800

    feat: 下载脚本 --source=auto

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 
 from modules.downloader.AutoModelDownloader import AutoModelDownloader
+from modules.downloader.dl_registry import HF_TEST_FILE_URL
+from modules.downloader.net import can_net_access
 
 try:
     logging.basicConfig(level=logging.INFO)
@@ -15,8 +17,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--source",
-        choices=["modelscope", "ms", "huggingface", "hf"],
-        required=True,
+        choices=["modelscope", "ms", "huggingface", "hf", "auto"],
+        # 默认 auto 自动根据网络环境选择，首选 huggingface
+        default="auto",
         help="Choose the source to download the model from",
     )
     # models 可以列出模型名字 逗号分割
@@ -31,5 +34,8 @@ if __name__ == "__main__":
         args.source = "modelscope"
     if args.source == "hf":
         args.source = "huggingface"
+    if args.source == "auto":
+        can_access_hf = can_net_access(HF_TEST_FILE_URL)
+        args.source = "huggingface" if can_access_hf else "modelscope"
     md = AutoModelDownloader(down_source=args.source)
     md.download_models(model_names=args.models.split(","), force=True)
